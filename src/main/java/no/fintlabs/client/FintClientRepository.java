f3e2abedb5846ab1e8d51b3cc7f21abda0d1f900
Trond Magnus Sevre <76692689+trondsevre@users.noreply.github.com>
2023-10-26 13:54:41 +0200

    Prevent duplicate client creation (#5)
    * Handle errors and emtpy object differently for client/adapter
    * Trigger update if password or clientSecret is missing
    * Prevent exception in encode/decode if password/secret is missing
    * Update dependecies

--- src/main/java/no/fintlabs/client/FintClientRepository.java
+++ src/main/java/no/fintlabs/client/FintClientRepository.java
@@ -72,22 +72,44 @@ public class FintClientRepository {
 
     public Set<Client> get(FintClientCrd crd) {
 
-        return getValueFromAnnotationByKey(crd, FintClientDependentResource.ANNOTATION_CLIENT_DN)
-                .map(dn -> clientEventRequestProducerService.get(ClientEvent
-                                .builder()
-                                .object(Client
-                                        .builder()
-                                        .dn(dn)
-                                        .publicKey(secretService.getPublicKeyString())
-                                        .build())
-                                .orgId(crd.getSpec().getOrgId())
-                                .operation(FintCustomerObjectEvent.Operation.READ)
-                                .build())
-                        .map(ClientEvent::getObject)
-                        .map(Collections::singleton)
-                        .orElse(Collections.emptySet())
-                )
-                .orElse(Collections.emptySet());
+        Optional<String> dn = getValueFromAnnotationByKey(crd, FintClientDependentResource.ANNOTATION_CLIENT_DN);
+        if (dn.isEmpty()) {
+            log.debug("Skipping client lookup due to missing DN in CRD.");
+            return Collections.emptySet();
+        }
+
+        log.debug("Getting client: {}", dn.get());
+        Optional<ClientEvent> responseOptional = clientEventRequestProducerService.get(createRequestEvent(crd, dn.get()));
+
+        if (responseOptional.isEmpty()) {
+            throw new CustomerObjectResponseException("Empty response from Kafka. The request has probably timed out. Client: " + dn.get());
+        }
+
+        ClientEvent response = responseOptional.get();
+
+        if (response.hasError()) {
+            throw new CustomerObjectResponseException(response.getErrorMessage());
+        }
+
+        if (response.getObject() == null) {
+            log.debug("Object in response is null");
+            return Collections.emptySet();
+        }
+
+        return Collections.singleton(response.getObject());
+    }
+
+    private ClientEvent createRequestEvent(FintClientCrd crd, String dn) {
+        return ClientEvent
+                .builder()
+                .object(Client
+                        .builder()
+                        .dn(dn)
+                        .publicKey(secretService.getPublicKeyString())
+                        .build())
+                .orgId(crd.getSpec().getOrgId())
+                .operation(FintCustomerObjectEvent.Operation.READ)
+                .build();
     }
 
     public void delete(Client client, FintClientCrd primay) {
